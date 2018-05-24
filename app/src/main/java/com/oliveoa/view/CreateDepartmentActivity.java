4b935abc76ceac8cee69b2492236d900664181ab
DementiaIsACat <m17876253479@163.com>
2018-05-24 22:11:00 +0800

    部门管理页面布局基本完成

---
+++ app/src/main/java/com/oliveoa/view/CreateDepartmentActivity.java
@@ -0,0 +1,14 @@
+package com.oliveoa.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.erica.oliveoa_company.R;
+public class CreateDepartmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_department);
+    }
+}
---
+++ app/src/main/java/com/oliveoa/view/NullDepartmentActivity.java
@@ -0,0 +1,18 @@
+package com.oliveoa.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.erica.oliveoa_company.R;
+
+public class NullDepartmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_null_department);
+    }
+
+}
